2fbac77b0e16e5a6972247c96059e312aee137d7
Yes526-tech <ysert20051@gmail.com>
2023-08-14 14:20:05 +0300

    created new xlsx, added  for loop

--- automation_python.py
+++ automation_python.py
@@ -4,8 +4,12 @@ wb = xl.load_workbook('transactions.xlsx')
 
 sheet = wb['Sheet1']
 
-cell = sheet['a1']
 
-cell = sheet.cell(1, 1)
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    print(cell.value)
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
 
-print(cell.value)
+wb.save('transactions2.xlsx')
